84be7492fbed30e295f977ffe917b9cd5135f8b9
musabrt <musafg@outlook.com>
2020-11-20 15:02:54 +0300

    NOs entegrated

--- src/com/egaku/input/mouse/events/BrushEvent.java
+++ src/com/egaku/input/mouse/events/BrushEvent.java
@@ -21,16 +21,24 @@ public class BrushEvent extends Mouse {
         if(e.getY() > 20){
             pressed = true;
             pane.render((g) -> {
-
+                lastx = e.getX();
+                lasty = e.getY();
                 g.setColor(new Color(90,170,250));
                 g.fillOval(e.getX()-9, e.getY()-9, 18,18);
             });
         }
     }
 
+    private int lastx;
+    private int lasty;
+
     @Override
     public void mouseReleased(MouseEvent e) {
         if (pressed) pressed = false;
+
+        lastx = Integer.MIN_VALUE;
+        lasty = Integer.MIN_VALUE;
+
     }
 
     @Override
@@ -38,7 +46,14 @@ public class BrushEvent extends Mouse {
         if (pressed) {
             pane.render((g) -> {
                 g.setColor(new Color(90,170,250));
-                g.fillOval(e.getX()-9, e.getY()-9, 18,18);
+                g.fillOval(e.getX()-8, e.getY()-8, 16,16);
+
+                if ((lastx != Integer.MIN_VALUE && lasty != Integer.MIN_VALUE) && (Math.abs(e.getX()-lastx) != 1 || Math.abs(e.getY()-lasty) != 1)) {
+                    g.setStroke(new BasicStroke(16f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+                    g.drawLine(lastx,lasty,e.getX(),e.getY());
+                    lastx=e.getX();
+                    lasty=e.getY();
+                }
             });
         }
     }
